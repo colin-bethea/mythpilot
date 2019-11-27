f640915b6bfbdd330590e0107ea835dd18242163
Colin Bethea <colinjbethea@gmail.com>
2019-11-27 15:32:10 -0500

    Add project data properties/props, map through data for view; if no projects, display a separate message

--- src/tables/ProjectTable.js
+++ src/tables/ProjectTable.js
@@ -1,25 +1,33 @@
 import React from 'react'
 
-const ProjectTable = () => (
-    <table>
-      <thead>
+const ProjectTable = props => (
+  <table>
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Date</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.projects.length > 0 ? (
+        props.projects.map(project => (
+          <tr key={project.id}>
+            <td>{project.title}</td>
+            <td>{project.date}</td>
+            <td>
+              <button className="button muted-button">Edit</button>
+              <button className="button muted-button">Delete</button>
+            </td>
+          </tr>
+        ))
+      ) : (
         <tr>
-          <th>Title</th>
-          <th>Date</th>
-          <th>Actions</th>
+          <td colSpan={3}>No projects</td>
         </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Title Data</td>
-          <td>Date Data</td>
-          <td>
-            <button className="button muted-button">Edit</button>
-            <button className="button muted-button">Delete</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  )
-  
-  export default ProjectTable
+      )}
+    </tbody>
+  </table>
+)
+
+export default ProjectTable
