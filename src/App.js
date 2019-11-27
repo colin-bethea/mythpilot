78a7b0876b08184ebe06a04f62fa2252bc99f23a
Colin Bethea <colinjbethea@gmail.com>
2019-11-27 15:31:17 -0500

    Create sample project object data; use React.useState for projectsData; add projects object data to component view

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react'
 import ProjectTable from './tables/ProjectTable'
 
 const App = () => {
+  const projectsData = [
+    { id: 1, title: 'Athenify', date: '8/24/19'},
+    { id: 2, title: 'Stratagem', date: '9/24/19'},
+    { id: 3, title: 'Coursebuild', date: '8/28/19'}
+  ]
+
+  const [projects] = React.useState(projectsData)
+
   return (
     <div className="container">
       <h1>ProjectSpec</h1>
@@ -11,7 +19,7 @@ const App = () => {
         </div>
         <div className="flex-large">
           <h2>View projects</h2>
-          <ProjectTable/>
+          <ProjectTable projects={projects} />
         </div>
       </div>
     </div>
